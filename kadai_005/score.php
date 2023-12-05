774393368383bfad9c6e2cdbd223d5b3a3f6a19a
ssmika <147523894+ssmika@users.noreply.github.com>
2023-12-05 22:04:28 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,20 @@
+<?php
+//10人分の点数//
+$score1 = 80;
+$score2 = 60;
+$score3 = 55;
+$score4 = 40;
+$score5 = 100;
+$score6 = 25;
+$score7 = 80;
+$score8 = 95;
+$score9 = 30;
+$score10 = 60;
+
+//合計//
+$total = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
+//平均/
+echo $total / 10;
+
+?>
