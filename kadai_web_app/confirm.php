e7fc37657e693e6a6d18563886238f5862e17f9f
ssmika <147523894+ssmika@users.noreply.github.com>
2023-12-13 23:14:48 +0900

    Add files via upload

---
+++ kadai_web_app/confirm.php
@@ -0,0 +1,47 @@
+<?php
+
+// POSTリクエストから入力データを取得
+$name = $_POST['semployee_name'];
+$age = $_POST['semployee_age'];
+$department = $_POST['department'];
+?>
+
+<!DOCTYPE>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP課題app</title>
+</head>
+
+<body>
+    <h2>入力内容をご確認ください。</h2>
+    <p>問題なければ「確定」、修正する場合は「キャンセル」をクリックしてください。</p>
+
+    <table border="1">
+        <tr>
+            <th>項目</th>
+            <th>入力内容</th>
+        </tr>
+        <tr>
+            <td>社員名
+            </td>
+            <td><?php echo $name; ?></td>
+        </tr>
+        <tr>
+            <td>年齢</td>
+            <td><?php echo $age; ?></td>
+        </tr>
+        <tr>
+            <td>所属部署</td>
+            <td><?php echo $department; ?></td>
+        </tr>
+    </table>
+
+    <p>
+        <button id="confirm-btn" onclick="location.href='complete.php';">確定</button>
+        <button id="cancel-btn" onclick="history.back();">キャンセル</button>
+    </p>    
+</body>
+
+</html>
